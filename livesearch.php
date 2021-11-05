4da381850c986f28360255e7b2d567e5b302cae3
DragonPyli <codewizard001@gmail.com>
2021-11-05 17:18:22 +0530

    Temp Livesearch

--- livesearch.php
+++ livesearch.php
@@ -11,8 +11,9 @@ $q=$_GET["q"];
 if (strlen($q)>0) {
   $hint="";
   for($i=0; $i<($x->length); $i++) {
-    $y=$x->item($i)->getElementsByTagName('title');
+    $y=$x->item($i)->getElementsByTagName('content');
     $z=$x->item($i)->getElementsByTagName('url');
+    $t=$x->item($i)->getElementsByTagName('title');
     if ($y->item(0)->nodeType==1) {
       //find a link matching the search text
       if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)) {
@@ -20,12 +21,12 @@ if (strlen($q)>0) {
           $hint="<a href='" .
           $z->item(0)->childNodes->item(0)->nodeValue .
           "' target='_blank'>" .
-          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+          $t->item(0)->childNodes->item(0)->nodeValue . "</a>";
         } else {
           $hint=$hint . "<br /><a href='" .
           $z->item(0)->childNodes->item(0)->nodeValue .
           "' target='_blank'>" .
-          $y->item(0)->childNodes->item(0)->nodeValue . "</a>";
+          $t->item(0)->childNodes->item(0)->nodeValue . "</a>";
         }
       }
     }
